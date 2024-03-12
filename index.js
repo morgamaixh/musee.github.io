2307cb4548ee4d0d27ee33100cc902d8e8df7a10
morgamaixh <161282837+morgamaixh@users.noreply.github.com>
2024-03-12 09:35:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+            const btn = document.getElementById("tgbtn");
+            const x = document.getElementById("link");
+            btn.addEventListener('click', () => {
+                x.classList.toggle('active');
+            });
+
+            function ridirect(){
+                window.location.href = "https://github.com/morgamaixh.github.io/miniprojects.github.io/";
+            }
+  
